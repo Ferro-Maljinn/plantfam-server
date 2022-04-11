209a0b9233c2bbd800d4c6d4f2939df766cfa488
Adalia da Silva <ada.future.work@gmail.com>
2022-04-11 23:04:24 +0200

    completed update

--- routes/UpdatePlant.routes.js
+++ routes/UpdatePlant.routes.js
@@ -2,16 +2,31 @@ const router = require("express").Router();
 const PlantModel = require("../models/Plant.model");
 
 /* GET updatePlant page */
-router.post("/updatePlant", async (req, res, next) => {
-    try{
-        const currentPlant = await PlantModel.findOne({ plantId: req.session.currentUser._id });
-        console.log(req.session.currentUser.plants._id, "currentplant")
-        res.json("All good from the updateplant route");
-    }
-    catch(err){
-        console.log(err, "error from update plant")
-    }
-  
+router.put("/update/plant/:id", async (req, res, next) => {
+  console.log("arrived in update plant");
+  try {
+    const { id } = req.params;
+    const { image, description, englishName, latinName,  height, light, watering, soilType} = req.body;
+    const currentPlant = await PlantModel.findOneAndUpdate(
+      id,
+      {
+        image: image,
+        description: description,
+        englishName: englishName,
+        latinName: latinName,
+        height: height,
+        light: light,
+        watering: watering,
+        soilType: soilType,
+      },
+      {
+        new: true,
+      }
+    );
+    return res.json(currentPlant);
+  } catch (err) {
+    console.log(err, "error from update plant");
+  }
 });
 
 module.exports = router;
